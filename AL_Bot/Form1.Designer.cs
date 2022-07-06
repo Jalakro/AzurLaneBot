fc017a245dc2b79e19d707f65eaa0ae756f488b3
Jalakro <a.poignet12@orange.fr>
2022-07-07 00:56:58 +0200

    add project in github repo

---
+++ AL_Bot/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace AL_Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.myTimer = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myTimer
+            // 
+            this.myTimer.Interval = 300;
+            this.myTimer.Tick += new System.EventHandler(this.myTimer_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoCheck = false;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(100, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(184, 25);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "AutoRunMap Running";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(130, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "AutoRunMap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 444);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(12, 439);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Get Pixel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoCheck = false;
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(239, 130);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(119, 19);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Secondary Screen";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoCheck = false;
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(39, 130);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(105, 19);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Primary Screen";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(150, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 35);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Swap Screen";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(347, 446);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(25, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "?";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoCheck = false;
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(130, 233);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(130, 19);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "Using Item If Found";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(130, 258);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(129, 30);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Use double Map Item";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 332);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 23);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(100, 361);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(83, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Launch";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(209, 361);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Stop";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(384, 481);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AzurLane Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer myTimer;
+        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private CheckBox checkBox4;
+        private Button button7;
+        private TextBox textBox1;
+        private Button button8;
+        private Button button9;
+    }
+}
---
+++ AL_Bot/Form1.cs
@@ -0,0 +1,465 @@
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Drawing;
+
+namespace AL_Bot
+{
+    public partial class Form1 : Form
+    {
+        bool MapFinishTest = false, DockFullTest = false, DoubleMapItemTest = false;
+        int xMax = 0, yMax = 0;
+        bool SecScreenIsPos = false;
+        int goalCounter = -1, goalNb;
+        Screen[] screens;
+
+        //données et fonction pour faire un click
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        //
+
+        //Get les pixels de l'écran
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        static extern Int32 ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+        //
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+
+            screens = System.Windows.Forms.Screen.AllScreens;
+            if (screens.Length < 2)
+            {
+                button4.Enabled = false;
+                checkBox3.Checked = true;
+                checkBox2.Checked = false;
+            }
+            else
+            {
+                if (screens[1].Bounds.X > 0)
+                {
+                    SecScreenIsPos = true;
+                }
+                else
+                {
+                    SecScreenIsPos = false;
+                }
+            }
+
+            if (checkBox3.Checked == true)
+            {
+                xMax = screens[0].Bounds.Width;
+                yMax = screens[0].Bounds.Height;
+            }
+            else
+            {
+                xMax = screens[1].Bounds.Width;
+                yMax = screens[1].Bounds.Height;
+            }
+
+        }
+
+        private void InitializeTimer()
+        {
+            // Set to 1 second  
+            myTimer.Interval = 1000;
+            myTimer.Tick += new EventHandler(myTimer_Tick);
+
+            // init timer state  
+            myTimer.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                myTimer.Enabled = false;
+                checkBox1.Checked = false;
+            }
+            else
+            {
+                myTimer.Enabled = true;
+                checkBox1.Checked = true;
+            }
+        }
+
+        public void DoMouseClick(int X, int Y)
+        {
+            int pre_height = Cursor.Clip.Size.Height, pre_width = Cursor.Clip.Size.Width, preXRect = Cursor.Clip.X, preYRect = Cursor.Clip.Y;
+            int pre_x = Cursor.Position.X, pre_y = Cursor.Position.Y;
+
+            if (xMax != 1920 && yMax != 1080)
+            {
+                X = X * xMax / 1920;
+                Y = Y * yMax / 1080;
+            }
+            
+            Rectangle BoundRect = new Rectangle(X, Y, 1, 1);
+            Cursor.Clip = BoundRect;
+
+            Thread.Sleep(100);
+            Cursor.Position = new Point(X, Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            Thread.Sleep(100);
+
+            Rectangle FullRect = new Rectangle(preXRect, preYRect, pre_width, pre_height);
+            Cursor.Clip = FullRect;
+
+            Cursor.Position = new Point(pre_x, pre_y);
+        }
+
+
+        static public System.Drawing.Color GetPixelColor(int x, int y)
+        {
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            uint pixel = GetPixel(hdc, x, y);
+            ReleaseDC(IntPtr.Zero, hdc);
+            Color color = Color.FromArgb((int)(pixel & 0x000000FF),
+                         (int)(pixel & 0x0000FF00) >> 8,
+                         (int)(pixel & 0x00FF0000) >> 16);
+            return color;
+        }
+
+        private bool CheckMapFinish()
+        {
+            Color PixelColor;
+            if(checkBox3.Checked == true)
+            {
+                PixelColor = GetPixelColor(1300, 900);
+            }
+            else
+            {
+                if (SecScreenIsPos)
+                {
+                    PixelColor = GetPixelColor(3220, 900);
+                }
+                else
+                {
+                    PixelColor = GetPixelColor(-620, 900);
+                }
+            }
+                
+            if (/*Avec meta*/PixelColor.R == 176 && PixelColor.G == 196 && PixelColor.B == 222 || /*Avec meta n°2*/PixelColor.R == 176 && PixelColor.G == 196 && PixelColor.B == 229 || /*Sans meta*/ PixelColor.R == 90 && PixelColor.G == 142 && PixelColor.B == 214)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool CheckDoubleItem()
+        {
+            Color PixelColor;
+            if (checkBox3.Checked == true)
+            {
+                PixelColor = GetPixelColor(815, 923);
+            }
+            else
+            {
+                if (SecScreenIsPos)
+                {
+                    PixelColor = GetPixelColor(2735, 923);
+                }
+                else
+                {
+                    PixelColor = GetPixelColor(-1105, 923);
+                }
+            }
+
+            if (PixelColor.R == 183 && PixelColor.G == 160 && PixelColor.B == 222)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool CheckDockFull()
+        {
+            Color PixelColor;
+            Color PixelColor2;
+            if (checkBox3.Checked == true)
+            {
+                PixelColor = GetPixelColor(1347, 338);
+                PixelColor2 = GetPixelColor(562, 712);
+            }
+            else
+            {
+                if (SecScreenIsPos)
+                {
+                    PixelColor = GetPixelColor(3267, 338);
+                    PixelColor2 = GetPixelColor(2482, 712);
+                }
+                else
+                {
+                    PixelColor = GetPixelColor(-573, 338);
+                    PixelColor2 = GetPixelColor(-1358, 712);
+                }
+            }
+            
+            if (PixelColor.R >= 177 && PixelColor.R <= 191 && PixelColor.G >= 142 && PixelColor.G <= 154 && PixelColor.B >= 153 && PixelColor.B <= 163 || PixelColor2.R == 57 && PixelColor2.G == 100 && PixelColor2.B == 156)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void myTimer_Tick(object sender, EventArgs e)
+        {
+            if(checkBox3.Checked == true)
+            {
+                xMax = screens[0].Bounds.Width;
+                yMax = screens[0].Bounds.Height;
+            }
+            else
+            {
+                xMax = screens[1].Bounds.Width;
+                yMax = screens[1].Bounds.Height;
+            }
+            
+
+            DockFullTest = CheckDockFull();
+            if (DockFullTest)
+            {
+                if(checkBox3.Checked == true)
+                {
+                    DoMouseClick(730, 750);
+                    Thread.Sleep(4000);
+                    DoMouseClick(1050, 970);
+                    Thread.Sleep(1500);
+                    DoMouseClick(1450, 930);
+                    Thread.Sleep(1500);
+                    DoMouseClick(1150, 720);
+                    Thread.Sleep(1500);
+                    DoMouseClick(1150, 720);
+                    Thread.Sleep(1500);
+                    DoMouseClick(1370, 790);
+                    Thread.Sleep(1500);
+                    DoMouseClick(1170, 830);
+                    Thread.Sleep(1500);
+                    DoMouseClick(1170, 830);
+                    Thread.Sleep(1500);
+                    DoMouseClick(1340, 960);
+                    Thread.Sleep(4000);
+                    DoMouseClick(1750, 800);
+                }
+                if(checkBox2.Checked == true)
+                {
+                    if (SecScreenIsPos)
+                    {
+                        DoMouseClick(2650, 750);
+                        Thread.Sleep(4000);
+                        DoMouseClick(2970, 970);
+                        Thread.Sleep(1500);
+                        DoMouseClick(3370, 930);
+                        Thread.Sleep(1500);
+                        DoMouseClick(3070, 720);
+                        Thread.Sleep(1500);
+                        DoMouseClick(3070, 720);
+                        Thread.Sleep(1500);
+                        DoMouseClick(3290, 790);
+                        Thread.Sleep(1500);
+                        DoMouseClick(3090, 830);
+                        Thread.Sleep(1500);
+                        DoMouseClick(3090, 830);
+                        Thread.Sleep(1500);
+                        DoMouseClick(3260, 960);
+                        Thread.Sleep(4000);
+                        DoMouseClick(3670, 800);
+                    }
+                    else
+                    {
+                        DoMouseClick(-1190, 750);
+                        Thread.Sleep(4000);
+                        DoMouseClick(-870, 970);
+                        Thread.Sleep(1500);
+                        DoMouseClick(-470, 930);
+                        Thread.Sleep(1500);
+                        DoMouseClick(-770, 720);
+                        Thread.Sleep(1500);
+                        DoMouseClick(-770, 720);
+                        Thread.Sleep(1500);
+                        DoMouseClick(-550, 790);
+                        Thread.Sleep(1500);
+                        DoMouseClick(-750, 830);
+                        Thread.Sleep(1500);
+                        DoMouseClick(-750, 830);
+                        Thread.Sleep(1500);
+                        DoMouseClick(-580, 960);
+                        Thread.Sleep(4000);
+                        DoMouseClick(-170, 800);
+                    }
+                }
+            }
+
+            MapFinishTest = CheckMapFinish();
+            if (MapFinishTest)
+            {
+                if(goalCounter >= 0)
+                {
+                    if (checkBox4.Checked == true)//If double map item wanted
+                    {
+                        DoubleMapItemTest = CheckDoubleItem();
+                        if (DoubleMapItemTest)
+                        {
+                            DoMouseClick(1089, 899);
+                            Thread.Sleep(1000);
+                        }
+                    }
+
+                    if (checkBox3.Checked == true)
+                    {
+                        DoMouseClick(1300, 900);
+                    }
+                    if (checkBox2.Checked == true)
+                    {
+                        if (SecScreenIsPos)
+                        {
+                            DoMouseClick(3220, 900);
+                        }
+                        else
+                        {
+                            DoMouseClick(-620, 900);
+                        }
+                    }
+                    goalCounter++;
+                    if (goalCounter >= goalNb)
+                    {
+                        myTimer.Enabled = false;
+                        button1.Enabled = true;
+                        goalCounter = -1;
+                        button8.Enabled = true;
+                        textBox1.Enabled = true;
+                        textBox1.Text = "";
+                    }
+                }
+                else
+                {
+                    if (checkBox4.Checked == true)//If double map item wanted
+                    {
+                        DoubleMapItemTest = CheckDoubleItem();
+                        if (DoubleMapItemTest)
+                        {
+                            DoMouseClick(1089, 899);
+                            Thread.Sleep(1000);
+                        }
+                    }
+
+                    if (checkBox3.Checked == true)
+                    {
+                        DoMouseClick(1300, 900);
+                    }
+                    if (checkBox2.Checked == true)
+                    {
+                        if (SecScreenIsPos)
+                        {
+                            DoMouseClick(3220, 900);
+                        }
+                        else
+                        {
+                            DoMouseClick(-620, 900);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Color tmp = GetPixelColor(Cursor.Position.X, Cursor.Position.Y);
+            MessageBox.Show("X :" + Cursor.Position.X + " Y :" + Cursor.Position.Y + " RGB :" + tmp.R + " " + tmp.G + " " + tmp.B);
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                checkBox2.Checked = true;
+                checkBox3.Checked = false;
+            }
+            else
+            {
+                checkBox2.Checked = false;
+                checkBox3.Checked = true;
+            }
+        }
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            bool successfullyParsed = int.TryParse(textBox1.Text, out goalNb);
+
+            if (successfullyParsed && goalNb > 0)
+            {
+                if(checkBox1.Checked == true)
+                {
+                    myTimer.Enabled = false;
+                    checkBox1.Checked = false;
+                }
+                goalCounter = 0;
+                button1.Enabled = false;
+                myTimer.Enabled = true;
+                button8.Enabled = false;
+                textBox1.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Texte entrée non valide");
+            }
+        }
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if(button8.Enabled == false && goalCounter >= 0)
+            {
+                myTimer.Enabled = false;
+                button1.Enabled = true;
+                goalCounter = -1;
+                button8.Enabled = true; 
+                textBox1.Enabled = true;
+                textBox1.Text = "";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Working Requirements :\n" + "-Device emulated must be the 'Samsung Galaxy S20 Ultra'\n" + "-Emulator window size must be entire window (not full screen, you must see the task bar)\n" + "-Quick retire must be configured");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if(checkBox4.Checked == true)
+            {
+                checkBox4.Checked = false;
+            }
+            else
+            {
+                checkBox4.Checked = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
